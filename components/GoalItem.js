e9ba74e84f2535f75788250fb0030c74f987a475
maokunsun <sun.maok@northeastern.edu>
2023-02-13 12:01:37 -0800

    use pressed boolean value in style prop for ios visual effect

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,15 +1,26 @@
 import { View, Text, StyleSheet, Button, Pressable } from "react-native";
 import React from "react";
+import PressableButton from "./PressableButton";
 
 export default function GoalItem({ goal, onDelete, onGoalItemPressed }) {
   return (
     <Pressable
+      style={(data) => {
+        // style={(pressed) => {
+        console.log("data from style", data);
+        return [
+          styles.textContainer,
+          data.pressed ? styles.pressedStyle : null,
+        ];
+      }}
       onPress={onGoalItemPressed}
-      android_ripple={{ color: "red", radius: "10" }}
+      // android_ripple={{ color: "red", radius: "10" }}
     >
       <View style={styles.textContainer}>
         <Text style={styles.text}>{goal.text}</Text>
         <Button color="black" title="X" onPress={() => onDelete(goal.id)} />
+
+        {/* <PressableButton /> */}
       </View>
     </Pressable>
   );
@@ -28,5 +39,9 @@ const styles = StyleSheet.create({
     color: "#4510ff",
     fontSize: 30,
     marginRight: 8,
+  },
+  pressedStyle: {
+    backgroundColor: "red",
+    opacity: 0.5,
   },
 });
---
+++ components/PressableButton.js
@@ -0,0 +1,17 @@
+// import { View, Text, Pressable } from "react-native";
+// import React from "react";
+// import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+// const PressableButton = (style,props) => {
+//   return (
+//     <Pressable style={{ pressed }} =>{} >
+//       <View>
+//         <Text>Button</Text>
+//         <MaterialCommunityIcons name="delete-forever" size={24} color="black" />
+//       </View>
+//     </Pressable>
+
+//   );
+// };
+
+// export default PressableButton;
